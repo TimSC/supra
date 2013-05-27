e18dde29f6f52dff6401ccb47e0e65c70ea963c9
Tim Sheerman-Chase <tim2009@sheerman-chase.org.uk>
2013-05-27 18:44:08 +0100

    Move training to separate file to aid pickle

--- supra.py
+++ supra.py
@@ -298,43 +298,9 @@ def TestTracker(cloudTracker, testNormSamples, log):
 	log.write(str(avCorrel)+","+str(avSignScore)+","+str(avPredError)+"\n")
 	log.flush()
 
+def TestSingleExample(cloudTracker, testNormSamples, log):
+	pass
+
 if __name__ == "__main__":
-
-	if 0:
-		normalisedSamples = LoadSamplesFromServer()
-		pickle.dump(normalisedSamples, open("normalisedSamples.dat","wb"), protocol=-1)
-	else:
-		normalisedSamples = pickle.load(open("normalisedSamples.dat","rb"))		
-
-	#Only use larger faces
-	filteredSamples = []
-	for sample in normalisedSamples:
-		if np.array(sample.model).std(axis=1)[0] > 15.:
-			filteredSamples.append(sample)
-
-	#DumpNormalisedImages(filteredSamples)
-
-	#Reduce problem to two points
-	for sample in filteredSamples:
-		sample.procShape = sample.procShape[0:1,:]
-
-	log = open("log.txt","wt")
-
-	while 1:
-		print "Filtered to",len(filteredSamples),"of",len(normalisedSamples),"samples"
-		halfInd = len(filteredSamples)/2
-		random.shuffle(filteredSamples)
-		trainNormSamples = filteredSamples[:halfInd]
-		testNormSamples = filteredSamples[halfInd:]
-
-		if 0:
-			cloudTracker = TrainTracker(trainNormSamples)
-			pickle.dump(cloudTracker, open("tracker.dat","wb"), protocol=-1)
-			pickle.dump(testNormSamples, open("testNormSamples.dat","wb"), protocol=-1)
-		else:
-			cloudTracker = pickle.load(open("tracker.dat","rb"))
-			testNormSamples = pickle.load(open("testNormSamples.dat","rb"))
-
-		#Run performance test
-		TestTracker(cloudTracker, testNormSamples, log)
+	pass
 
---
+++ supraTrain.py
@@ -0,0 +1,49 @@
+
+import supra, pickle, random
+import numpy as np
+
+if __name__ == "__main__":
+
+	if 0:
+		normalisedSamples = LoadSamplesFromServer()
+		pickle.dump(normalisedSamples, open("normalisedSamples.dat","wb"), protocol=-1)
+	else:
+		normalisedSamples = pickle.load(open("normalisedSamples.dat","rb"))		
+
+	#Only use larger faces
+	filteredSamples = []
+	for sample in normalisedSamples:
+		if np.array(sample.model).std(axis=1)[0] > 15.:
+			filteredSamples.append(sample)
+
+	#DumpNormalisedImages(filteredSamples)
+
+	#Reduce problem to two points
+	for sample in filteredSamples:
+		sample.procShape = sample.procShape[0:1,:]
+
+	log = open("log.txt","wt")
+
+	while 1:
+		print "Filtered to",len(filteredSamples),"of",len(normalisedSamples),"samples"
+		halfInd = len(filteredSamples)/2
+		random.shuffle(filteredSamples)
+		trainNormSamples = filteredSamples[:halfInd]
+		testNormSamples = filteredSamples[halfInd:]
+
+		if 1:
+			cloudTracker = supra.TrainTracker(trainNormSamples)
+			print cloudTracker
+			pickle.dump(cloudTracker, open("tracker.dat","wb"), protocol=-1)
+			pickle.dump(testNormSamples, open("testNormSamples.dat","wb"), protocol=-1)
+		else:
+			cloudTracker = pickle.load(open("tracker.dat","rb"))
+			testNormSamples = pickle.load(open("testNormSamples.dat","rb"))
+			print cloudTracker
+
+		#Run performance test
+		supra.TestTracker(cloudTracker, testNormSamples, log)
+
+		#TestSingleExample(cloudTracker, testNormSamples, log)
+
+
