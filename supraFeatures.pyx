6284d742506e31d4e85ba6199f6ee18e9c88b21f
Tim Sheerman-Chase <tim2009@sheerman-chase.org.uk>
2013-06-16 23:57:11 +0100

    Modularise sobel filter

--- supraFeatures.pyx
+++ supraFeatures.pyx
@@ -30,22 +30,45 @@ class FeatureIntSupport:
 	def GetFeatureList(self):
 		return ["int"+str(num) for num in range(self.numSupportPix)]
 
+class FeatureSobel:
+	def __init__(self, supportPixHalfWidth, numSupportPix=50):
+		self.supportPixOffSobel = np.random.uniform(low=-supportPixHalfWidth, \
+			high=supportPixHalfWidth, size=(numSupportPix, 2))
+		self.model = None
+		self.sample = None
+		self.numSupportPix = numSupportPix
+		self.sobelSample = None
+
+	def Gen(self, ptNum, xOff, yOff):
+		if self.sobelSample is None:
+			self.sobelSample = normalisedImageOpt.KernelFilter(self.sample)
+		pixSobel = ExtractSupportIntensity(self.sobelSample, self.supportPixOffSobel, \
+			self.model[ptNum][0], self.model[ptNum][1], xOff, yOff)
+		pixConvSobel = []
+		for px in pixSobel:
+			pixConvSobel.extend(px)
+		return pixConvSobel
+
+	def GetFeatureList(self):
+		return ["sob"+str(num) for num in range(self.numSupportPix)]
+	
 class FeatureGen:
 	def __init__(self, supportPixHalfWidth, numSupportPix=50):
 		self.sample = None
 		self.feat = None
 		self.featureIntSupport = FeatureIntSupport(supportPixHalfWidth, numSupportPix)
-		self.supportPixOffSobel = np.random.uniform(low=-supportPixHalfWidth, \
-			high=supportPixHalfWidth, size=(numSupportPix, 2))
+		self.sobelGen = FeatureSobel(supportPixHalfWidth, numSupportPix)
 		self.featureMask = None
 
 	def SetImage(self, img):
 		self.sample = img
 		self.featureIntSupport.sample = img
+		self.sobelGen.sample = img
 
 	def SetModel(self, model):
 		self.model = model
 		self.featureIntSupport.model = model
+		self.sobelGen.model = model
 
 	def SetPrevFrameFeatures(self, prevFeat):
 		self.prevFrameFeatures = prevFeat
@@ -65,16 +88,6 @@ class FeatureGen:
 	def SetOffset(self, offX, offY):
 		self.xOff = offX
 		self.yOff = offY
-
-	def GenSobelSupport(self, ptNum, xOff, yOff):
-		sobelSample = normalisedImageOpt.KernelFilter(self.sample)
-		pixSobel = ExtractSupportIntensity(sobelSample, self.supportPixOffSobel, \
-			self.model[ptNum][0], self.model[ptNum][1], xOff, yOff)
-		pixConvSobel = []
-		for px in pixSobel:
-			pixConvSobel.extend(px)
-
-		return pixConvSobel
 
 	def GenHog(self, ptNum, xOff, yOff):
 		imLocs = normalisedImageOpt.GenPatchOffsetList(self.model[ptNum][0]+xOff, self.model[ptNum][1]+yOff)
@@ -101,14 +114,12 @@ class FeatureGen:
 	def Gen(self):
 
 		self.pixGreyNorm = self.featureIntSupport.Gen(self.ptNum, self.xOff, self.yOff)
-		#print pixGreyNorm.shape
-		pixNormSobel = self.GenSobelSupport(self.ptNum, self.xOff, self.yOff)
-		#print pixNormSobel.shape
+		self.pixNormSobel = self.sobelGen.Gen(self.ptNum, self.xOff, self.yOff)
 		hog = self.GenHog(self.ptNum, self.xOff, self.yOff)
-		#print hog.shape
 		relDist = self.GenDistancePairs(self.ptNum, self.xOff, self.yOff)
 
-		self.feat = np.concatenate([hog, self.prevFrameFeatures, pixNormSobel, relDist])
+		self.feat = np.concatenate([hog, self.prevFrameFeatures, relDist])
+		print self.feat.shape
 
 		if self.featureMask is None:
 			return self.feat
@@ -130,6 +141,9 @@ class FeatureGen:
 				return self.feat[cnum]
 			if prefix == "int":
 				return self.pixGreyNorm[cnum]
+			if prefix == "sob":
+				return self.pixNormSobel[cnum]
+
 		else:
 			return self.feat[ind]
 
@@ -142,8 +156,9 @@ class FeatureGen:
 		self.featureMask = mask
 
 	def GetFeatureList(self):
-		comp = ["aaa"+str(i) for i in range(264)]
+		comp = ["aaa"+str(i) for i in range(114)]
 		comp.extend(self.featureIntSupport.GetFeatureList())
+		comp.extend(self.sobelGen.GetFeatureList())
 		return comp
 
 class FeatureGenPrevFrame:
